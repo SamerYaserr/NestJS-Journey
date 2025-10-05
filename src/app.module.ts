efa1ef62a9a6b59d772fd4c46ff141261e749123
SamerYaserr <samerzagloul@std.mans.edu.eg>
2025-10-05 20:07:57 +0300

    middlewre

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
-import { Module } from '@nestjs/common';
+import {
+  Module,
+  NestModule,
+  MiddlewareConsumer,
+  RequestMethod,
+} from '@nestjs/common';
+import cors from 'cors';
 import { CatModule } from './cats/cats.module';
+import { LoggerMiddleware } from './middleware/logger.middleware';
+import { CatController } from './cats/cats.controller';
 
 @Module({
   imports: [CatModule],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(cors(), LoggerMiddleware)
+      .exclude(
+        { path: 'cats', method: RequestMethod.GET },
+        { path: 'cats', method: RequestMethod.POST },
+      )
+      .forRoutes(CatController);
+  }
+}
